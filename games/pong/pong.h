3b33673dcfc868e0362bcdeb0acabcbbc389e3f3
IronMan1405 <daksheshnankani1405@gmail.com>
2026-02-25 19:09:35 +0530

    added pong

--- games/pong/pong.h
+++ games/pong/pong.h
@@ -1,6 +1,15 @@
 #ifndef PONG_H
 #define PONG_H
 
+typedef enum {
+    PONG_TITLE, PONG_PLAYING, PONG_PAUSE, PONG_OVER
+} PongStates;
+
+typedef struct {
+    float x;
+    float y;
+} Coords;
+
 void initPong(void);
 void updatePong(void);
 void drawPong(void);
